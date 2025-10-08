913060440d67407ef0b8951a2fb204e5e7a68492
denidadaev <165592822+denidadaev@users.noreply.github.com>
2025-10-08 16:47:16 +0200

    GeneradorAlias.java

--- src/main/java/com/ieschabas/GeneradorAlias.java
+++ src/main/java/com/ieschabas/GeneradorAlias.java
@@ -1,6 +1,6 @@
 package com.ieschabas;
 /**
- * Clase que me crea una ficha con mi nombre, mi edad calculada y donde vivo
+ * Clase que genera un alias a partir de las primeras letras del nombre y las letras del ciclo introducido
  * @author Deni Dadaev
  * @version 1.0
  */
